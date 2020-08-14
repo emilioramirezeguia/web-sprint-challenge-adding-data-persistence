1eb2537f58a9329dd5202843b0b310b7d12f652f
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-08-14 11:28:54 -0700

    POST a new task

--- projects/projectsModel.js
+++ projects/projectsModel.js
@@ -2,7 +2,9 @@ const db = require("../data/db-config");
 
 module.exports = {
   getProjects,
+  getProjectById,
   addProject,
+  addTask,
 };
 
 // Select every project and bring back all of its data
@@ -10,19 +12,20 @@ function getProjects() {
   return db.select("*").from("projects");
 }
 
+// Select project by id
+function getProjectById(id) {
+  return db("projects").where({ id }).first();
+}
+
 // Add a project and bring back the newly created project
 function addProject(project) {
   return db("projects").insert(project);
+}
 
-  //   return db("projects")
-  //     .insert(project)
-  //     .then((projectIDS) => {
-  //       if (projectIDS.length === 1) {
-  //         return projectsIDS[0];
-  //       } else if (projectIDS.length > 1) {
-  //         return projectIDS;
-  //       } else {
-  //         return null;
-  //       }
-  //     });
+function addTask(projectID, task) {
+  const newTask = {
+    ...task,
+    projectID: projectID,
+  };
+  return db("tasks").insert(newTask);
 }
--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -14,6 +14,20 @@ router.get("/", (req, res) => {
     });
 });
 
+// GET project by id
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  console.log("Project ID: ", id);
+
+  Projects.getProjectById(id)
+    .then((project) => {
+      res.status(200).json(project);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
+
 // Create (POST) a project
 router.post("/", (req, res) => {
   const project = req.body;
@@ -28,5 +42,17 @@ router.post("/", (req, res) => {
 });
 
 // Create (POST) a task for a project
+router.post("/:id/tasks", (req, res) => {
+  const task = req.body;
+  const projectID = req.params.id;
+
+  Projects.addTask(projectID, task)
+    .then((task) => {
+      res.status(201).json(task);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to create new task" });
+    });
+});
 
 module.exports = router;
